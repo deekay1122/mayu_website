77dca547b368e791cfb3db358a751ae9ce1854de
deekay1122 <deekay1122@gmail.com>
2021-06-12 15:37:42 +0900

    email

--- app/Http/Controllers/OnlineSalonController.php
+++ app/Http/Controllers/OnlineSalonController.php
@@ -11,22 +11,23 @@ class OnlineSalonController extends Controller
     }
 
     public function createProduct() {
+        $token = env('PAYPAL_SANDBOX_ACCESS_TOKEN', null);
+        $clientId = env('PAYPAL_SANDBOX_CLIENT_ID', null);
+        $secret = env('PAYPAL_SANDBOX_CLIENT_SECRET', null);
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "https://api-m.sandbox.paypal.com/v1/catalogs/products");
+        curl_setopt($curl, CURLOPT_URL, "https://api-m.sandbox.paypal.com/v1/oauth2/token");
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_HTTPHEADER, [
-            "Content-Type: application/json",
-            "Authorization: Bearer EKTQACO6Gq7rt99Ep9_XhQkM2JWaxjP9EB03K9Hi1JAAR21EfhbHZDURxzyxunW4HfuXzmRlvjl85Qhp",
-            "PayPal-Request-Id: PRODUCT-18062020-001"
+            "Accept: application/json",
+            "Accept-Language: en_US"
         ]);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, [
-            "name : Video Streaming Service",
-            "description : A video streaming service",
-            "type : SERVICE",
-            "category : SOFTWARE",
-            "image_url : https://example.com/streaming.jpg",
-            "home_url : https://example.com/home"
-        ]);
+        curl_setopt($curl, CURLOPT_USERPWD, $clientId.":".$secret);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // returns string == true
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // doesnt check ssl certs
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); // doesnt check ssl certs
+
+        // curl_setopt($curl, CURLOPT_SSLVERSION , 6);
 
         $result = curl_exec($curl);
         dd($result);
@@ -46,6 +47,6 @@ class OnlineSalonController extends Controller
     }
 
     public function paypalSubscriptionListener() {
-        
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\OnlineSalonController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +23,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
@@ -36,3 +38,20 @@ Route::get('/store_order', [ShopController::class, 'storeOrder'])->middleware('a
 Route::get('/online_salon', [OnlineSalonController::class, 'showOnlineSalon']);
 Route::get('/online_salon/createProduct', [OnlineSalonController::class, 'createProduct']);
 Route::post('online_salon/webhook', [OnlineSalonController::class, 'paypalSubscriptionListener']);
+
+// email
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');

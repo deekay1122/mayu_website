ab7a2a480027d8ad6274d71698ea12e36e2eb3ac
deekay1122 <deekay1122@gmail.com>
2021-06-13 23:12:12 +0900

    webhook

--- app/Http/Controllers/OnlineSalonController.php
+++ app/Http/Controllers/OnlineSalonController.php
@@ -43,6 +43,6 @@ class OnlineSalonController extends Controller
     }
 
     public function paypalSubscriptionWebhookListener(Request $request) {
-        echo $request->resource->id;
+        echo $request->resource;
     }
 }

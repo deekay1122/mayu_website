5290e4954e12a73aada4a68089dbe692f571a3c5
deekay1122 <deekay1122@gmail.com>
2021-06-14 23:12:33 +0900

    webhook &  dashboard for the online salon

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,13 +4,25 @@
     <p>オンラインサロン参加状況</p>
     <div class="online_salon_status">
         @if ($subscription == null)
-            <p>未参加</p>
+            <p>未参加　下記ボタンからお申し込みください</p>
             <div class="paypal_subscription_container">
                 <div class="paypal_subscription" id="paypal-button-container-P-5P000947JX925431MMDC5CYI"></div>
             </div>        
+        @elseif ($subscription->status == null or $subscription->status == "APPROVAL_PENDING" or $subscription->status == "APPROVED")
+            <p>参加お申し込みの処理中</p>
+        @elseif ($subscription->status == "SUSPENDED")
+            <p>参加は停止中です。管理者にお問い合わせください。</p>
+        @elseif ($subscription->status == "CANCELLED")
+            <p>参加はキャンセルされております。再度お申し込みください。</p>
+            <div class="paypal_subscription_container">
+                <div class="paypal_subscription" id="paypal-button-container-P-5P000947JX925431MMDC5CYI"></div>
+            </div> 
         @else
             <p>参加中</p>
         @endif
+        @endif
+
+        
     </div>
 </div> 
 <script src="https://www.paypal.com/sdk/js?client-id={{ env('PAYPAL_SANDBOX_CLIENT_ID') }}&vault=true&intent=subscription" data-sdk-integration-source="button-factory"></script> 
